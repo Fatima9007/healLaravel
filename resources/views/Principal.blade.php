0c9349793fa8a73baa0a7db1a2ab1262549c8d73
JosueRed4 <josue.isaa@gmail.com>
2020-04-16 20:29:50 -0500

    Version 3.6.1 Actualizacion del Perfil

--- resources/views/Principal.blade.php
+++ resources/views/Principal.blade.php
@@ -1,7 +1,56 @@
 @extends('web')
 
 @section('content')
-<div>
-    <h1>Lista de Paciente</h1>
-</div>
+<center>
+    <div class="col-md-10">
+
+            <table class="table">
+            <thead class="thead-dark">
+                <tr>
+                <th scope="col">Paciente</th>
+
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <th scope="row">Paciente</th>
+                </tr>
+
+                <tr>
+                    <th scope="row">Paciente</th>
+                    
+                </tr>
+
+                <tr>
+                    <th scope="row">Paciente</th>
+         
+                </tr>
+        
+                <tr>
+                    <th scope="row">Paciente</th>
+                </tr>
+
+                <tr>
+                    <th scope="row">Paciente</th>
+                </tr>
+
+                <tr>
+                    <th scope="row">Paciente</th>
+                </tr>
+
+                <tr>
+                    <th scope="row">Paciente</th>
+                </tr>
+                <tr>
+                    <th scope="row">Paciente</th>
+                </tr>
+
+                <tr>
+                    <th scope="row">Paciente</th>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    </center>
 @endsection
